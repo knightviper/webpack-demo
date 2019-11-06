b5a458a8c2b84d29aee851aa4fcfd139f27d6394
Ashik Shrestha <ashikhaizu5@gmail.com>
2019-11-07 00:36:12 +0530

    installed nodemon to watch the changes in webpack.config.js file and ignored node_modules

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,17 @@ module.exports = {
         port: process.env.PORT, //Defaults to 8080
         open: true, //Open the page in browser
         overlay: true, //Captures compilation related warnings and errors
+        watchOptions: {
+            aggregateTimeout: 300,
+            poll: 1000,
+        },
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: "Webpack Demo",
         }),
+        new webpack.WatchIgnorePlugin([
+            path.join(__dirname, "node_modules")
+        ]),
     ],
 };
