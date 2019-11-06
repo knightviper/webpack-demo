287a7cf84a4023c384cc62a91bd7c4d9bee18aba
Ashik Shrestha <ashikhaizu5@gmail.com>
2019-11-07 00:22:27 +0530

    configuring WDS through webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    devServer: {
+        stats: "errors-only", //Displays only errors to reduce the amount of output
+        host: process.env.HOST, //Defaults to localhost
+        port: process.env.PORT, //Defaults to 8080
+        open: true, //Open the page in browser
+        overlay: true, //Captures compilation related warnings and errors
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: "Webpack Demo",
